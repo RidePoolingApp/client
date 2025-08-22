b1c4e8d6f6d7ec3d1322f40ff8ff4f22c3cbb3dc
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-08-22 10:26:56 +0530

    implemented emial pass auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
-import { Stack } from "expo-router";
-
+import { Tabs } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
+import { TabBarBG } from "@/components/TabBarBG";
+import { Platform } from "react-native";
 export default function RootLayout() {
-  return <Stack />;
+	return <Tabs >
+		<Tabs.Screen name="(tabs)/index" options={{
+			title: ' My Home', tabBarLabel: 'Home', tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
+			tabBarActiveTintColor: "#FFBF00", tabBarShowLabel: false, tabBarBackground: TabBarBG, tabBarStyle: Platform.select({
+				android: {
+					position: "absolute",
+					bottom: 0,
+					left: 0,
+					right: 0,
+					shadowOpacity: 0,
+					elevation: 2
+				}
+			})
+
+		}} />
+		<Tabs.Screen name="index" options={{
+			title: 'Auth', tabBarLabel: 'Home', tabBarIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} />,
+			tabBarActiveTintColor: "#FFBF00", tabBarShowLabel: false, tabBarBackground: TabBarBG, tabBarStyle: Platform.select({
+				android: {
+					position: "absolute",
+					bottom: 0,
+					left: 0,
+					right: 0,
+					shadowOpacity: 0,
+					elevation: 2
+				}
+			})
+
+		}} />
+	</Tabs>
 }
